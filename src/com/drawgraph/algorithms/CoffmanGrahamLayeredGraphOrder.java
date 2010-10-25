7bf0dcea161974ae76f59b84138d30e92e1cac55
Denis Knjazhev <denis.k1985@gmail.com>
2010-10-25 18:04:50 +0300

    started CoffmanGraham implementation

--- src/com/drawgraph/algorithms/CoffmanGrahamLayeredGraphOrder.java
+++ src/com/drawgraph/algorithms/CoffmanGrahamLayeredGraphOrder.java
@@ -3,7 +3,11 @@ package com.drawgraph.algorithms;
 import com.drawgraph.model.Graph;
 import com.drawgraph.model.Node;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Date: Oct 25, 2010
@@ -13,6 +17,12 @@ import java.util.List;
  */
 public class CoffmanGrahamLayeredGraphOrder implements LayeredGraphOrder<Node> {
 	private int layerLength;
+
+	private static final int EQUAL = 0;
+	private static final int MORE = 1;
+	private static final int LESS = -1;
+
+	HashMap<Node, Integer> labels = new HashMap<Node, Integer>();
 	public CoffmanGrahamLayeredGraphOrder(int value) {
 		layerLength = value;
 	}
@@ -24,11 +34,59 @@ public class CoffmanGrahamLayeredGraphOrder implements LayeredGraphOrder<Node> {
 
 	@Override
 	public List<List<Node>> getLayers(Graph<Node> g) {
-		return null;  //todo implement this!
+		HashSet<Node> notSourcesNodes = new HashSet<Node>();
+		int label = 1;
+		for(Node n:	g.getNodes()) {
+			if (n.getSources().isEmpty()) {
+				labels.put(n, label);
+				label++;
+			} else {
+				notSourcesNodes.add(n);
+			}
+		}
+
+		//todo
+		return null;
 	}
 
 	@Override
 	public void setLayerLength(int layerLength) {
 		this.layerLength = layerLength;
+	}
+
+	private int lexicalComparison(Set<Node> first, Set<Node> second) {
+		final int firstSize = first.size();
+		final int secondSize = second.size();
+
+		int result;
+		if (firstSize > 0 && secondSize == 0) {
+			result = MORE;
+		} else if (firstSize == 0 && secondSize > 0) {
+			result = LESS;
+		} else if (firstSize == 0 && secondSize == 0) {
+			result = EQUAL;
+		} else {
+			Map.Entry<Node, Integer> maxFirstLabel = getLabelWithMaxLabel(first);
+			Map.Entry<Node, Integer> maxSecondLabel = getLabelWithMaxLabel(second);
+			if (maxFirstLabel.getValue() > maxSecondLabel.getValue()) {
+				result = MORE;
+			} else if (maxFirstLabel.getValue() < maxSecondLabel.getValue()) {
+				result = LESS;
+			} else  {
+				HashSet<Node> newFirstSet = new HashSet<Node>(first);
+				HashSet<Node> newSecondSet = new HashSet<Node>(second);
+
+				newFirstSet.remove(maxFirstLabel.getKey());
+				newSecondSet.remove(maxSecondLabel.getKey());
+				result = lexicalComparison(newFirstSet, newSecondSet);
+			}
+		}
+
+		return result;
+	}
+
+	private Map.Entry<Node, Integer> getLabelWithMaxLabel(Set<Node> nodes) {
+		//todo
+		return null;
 	}
 }
